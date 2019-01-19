1b736844ef04ff9cee4f522524e3c850a1ffd50a
IvanBlacky <tcherivan@gmail.com>
2019-01-19 18:59:43 +0300

    Refactor: rename module to "stocks"

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express"),
   app = express(),
-  market = require("./src/market"),
+  market = require("./src/stocks"),
   PORT = process.env.PORT || 1337;
 
 app.use(express.static("static"));

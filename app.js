c1683fa58058f18ba2a1d8310fe1b3020914a875
IvanBlacky <tcherivan@gmail.com>
2019-01-21 14:20:05 +0300

    Feat: add some profile endpoints

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
 const express = require("express"),
   app = express(),
   market = require("./src/stocks"),
+  profile = require("./src/profile"),
+  bodyParser = require("body-parser"),
   PORT = process.env.PORT || 1337;
 
 app.use(express.static("static"));
+app.use(bodyParser.json());
 
 app.get("/api/stocks", (req, res, next) => {
   const history = market.history;
   res.send(history);
+});
+
+app.post("/api/profile/create", async (req, res, next) => {
+  let newProfile = {};
+  try {
+    newProfile = await profile.registerNew({ ...req.body });
+    res.send(newProfile.toJSON());
+  } catch (e) {
+    console.error(e);
+    res.status(e.code || 500);
+    res.send(e);
+  }
+});
+
+app.post("/api/profile/check-password", async (req, res, next) => {
+  let resp = {};
+  try {
+    resp = await profile.checkPassword({ ...req.body });
+    res.send(resp);
+  } catch (e) {
+    console.error(e);
+    res.status(e.code || 500);
+    res.send(e);
+  }
+});
+
+app.post("/api/profile/convert", async (req, res, next) => {
+  // TODO: make call authorization-based
+  const user = await profile.findOne({ username: req.body.username });
+
+  try {
+    const resp = await user.convertCurrency({
+      ...req.body,
+      stocks: market.history[market.history.length - 1]
+    });
+    res.send(resp.toJSON());
+  } catch (e) {
+    console.error(e);
+    res.status(e.code || 500);
+    res.send(e);
+  }
 });
 
 app.listen(PORT, () => {

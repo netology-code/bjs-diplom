285efb6dec1ea801e186470edd73c7f3fd2a380f
IvanBlacky <tcherivan@gmail.com>
2019-01-21 16:27:59 +0300

    Feat: add authorization and some endpoints

--- app.js
+++ app.js
@@ -2,11 +2,29 @@ const express = require("express"),
   app = express(),
   market = require("./src/stocks"),
   profile = require("./src/profile"),
+  jwt = require("./src/jwt"),
   bodyParser = require("body-parser"),
+  cookieParser = require("cookie-parser"),
   PORT = process.env.PORT || 1337;
+
+const authenticate = async (req, res, next) => {
+  try {
+    const token = req.cookies.authorization;
+    const user = await jwt.verify(token);
+    req._user = await profile.findById(user._doc._id);
+    next();
+  } catch (e) {
+    console.error(e);
+    res.status(403).send({
+      code: 403,
+      message: "Authentication failure"
+    });
+  }
+};
 
 app.use(express.static("static"));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get("/api/stocks", (req, res, next) => {
   const history = market.history;
@@ -25,11 +43,13 @@ app.post("/api/profile/create", async (req, res, next) => {
   }
 });
 
-app.post("/api/profile/check-password", async (req, res, next) => {
-  let resp = {};
+app.post("/api/profile/login", async (req, res, next) => {
   try {
-    resp = await profile.checkPassword({ ...req.body });
-    res.send(resp);
+    await profile.checkPassword({ ...req.body });
+    const user = await profile.findOne({ username: req.body.username });
+    const token = await jwt.sign({ ...user });
+    res.setHeader("Set-Cookie", `authorization=${token}`);
+    res.sendStatus(204);
   } catch (e) {
     console.error(e);
     res.status(e.code || 500);
@@ -37,16 +57,30 @@ app.post("/api/profile/check-password", async (req, res, next) => {
   }
 });
 
-app.post("/api/profile/convert", async (req, res, next) => {
-  // TODO: make call authorization-based
-  const user = await profile.findOne({ username: req.body.username });
+app.post("/api/profile/convert", authenticate, async (req, res, next) => {
+  const user = req._user;
 
   try {
     const resp = await user.convertCurrency({
       ...req.body,
+      ...user,
+
       stocks: market.history[market.history.length - 1]
     });
     res.send(resp.toJSON());
+  } catch (e) {
+    console.error(e);
+    res.status(e.code || 500);
+    res.send(e);
+  }
+});
+
+app.post("/api/profile/transfer", authenticate, async (req, res, next) => {
+  const user = req._user;
+
+  try {
+    const resp = await user.transfer({ ...req.body });
+    res.send(resp);
   } catch (e) {
     console.error(e);
     res.status(e.code || 500);

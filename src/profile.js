2903758b16676acd831aefa935355603bff6becf
IvanBlacky <tcherivan@gmail.com>
2019-01-29 12:06:47 +0300

    Style: apply prettier

--- src/profile.js
+++ src/profile.js
@@ -1,186 +1,205 @@
-const mongoose = require("mongoose"),
-  createHash = require("create-hash"),
-  cryptoRandomString = require("crypto-random-string");
+const mongoose = require('mongoose'),
+    createHash = require('create-hash'),
+    cryptoRandomString = require('crypto-random-string');
 
 mongoose.connect(
-  "mongodb://localhost:27017/netology",
-  {
-    useNewUrlParser: true
-  }
+    'mongodb://localhost:27017/netology',
+    {
+        useNewUrlParser: true,
+    }
 );
 
 const ProfileSchema = new mongoose.Schema({
-  name: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true }
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  salt: {
-    type: String,
-    required: true
-  },
-  wallet: {
-    RUB: {
-      type: Number,
-      default: 0
+    name: {
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
     },
-    USD: {
-      type: Number,
-      default: 0
+    username: {
+        type: String,
+        required: true,
     },
-    EUR: {
-      type: Number,
-      default: 0
+    password: {
+        type: String,
+        required: true,
     },
-    NETCOIN: {
-      type: Number,
-      default: 0
-    }
-  }
+    salt: {
+        type: String,
+        required: true,
+    },
+    wallet: {
+        RUB: {
+            type: Number,
+            default: 0,
+        },
+        USD: {
+            type: Number,
+            default: 0,
+        },
+        EUR: {
+            type: Number,
+            default: 0,
+        },
+        NETCOIN: {
+            type: Number,
+            default: 0,
+        },
+    },
 });
 
 ProfileSchema.methods.addMoney = async function({ currency, amount }) {
-  const allowedCurrencies = ["RUB", "USD", "EUR"];
-  if (allowedCurrencies.indexOf(currency) === -1) {
-    throw {
-      code: 400,
-      message: `Currency ${currency} is not one of allowed currencies: ${allowedCurrencies}`
+    const allowedCurrencies = ['RUB', 'USD', 'EUR'];
+    if (allowedCurrencies.indexOf(currency) === -1) {
+        throw {
+            code: 400,
+            message: `Currency ${currency} is not one of allowed currencies: ${allowedCurrencies}`,
+        };
+    }
+    this.wallet[currency] = Number(this.wallet[currency]) + Number(amount);
+    const savedProfile = await this.save();
+    return {
+        name: savedProfile.name,
+        wallet: savedProfile.wallet,
+        username: savedProfile.username,
     };
-  }
-  this.wallet[currency] = Number(this.wallet[currency]) + Number(amount);
-  return await this.save();
 };
 
 ProfileSchema.methods.checkAmount = function(currency) {
-  const allowedCurrencies = ["RUB", "USD", "EUR", "NETCOIN"];
-  if (allowedCurrencies.indexOf(currency) === -1) {
-    throw {
-      code: 400,
-      message: `Currency ${currency} is not one of allowed currencies: ${allowedCurrencies}`
-    };
-  }
-  return this.wallet[currency];
+    const allowedCurrencies = ['RUB', 'USD', 'EUR', 'NETCOIN'];
+    if (allowedCurrencies.indexOf(currency) === -1) {
+        throw {
+            code: 400,
+            message: `Currency ${currency} is not one of allowed currencies: ${allowedCurrencies}`,
+        };
+    }
+    return this.wallet[currency];
 };
 
 ProfileSchema.methods.transfer = async function({ to, amount }) {
-  const toUser = await Profile.findOne({ username: to });
-  if (!toUser) {
-    throw {
-      code: 404,
-      message: `No user with such username: ${to}`
-    };
-  }
+    const toUser = await Profile.findOne({ username: to });
+    if (!toUser) {
+        throw {
+            code: 404,
+            message: `No user with such username: ${to}`,
+        };
+    }
 
-  const isEnoughMoney = this.checkAmount("NETCOIN");
+    const isEnoughMoney = this.checkAmount('NETCOIN');
 
-  if (!isEnoughMoney) {
-    throw {
-      code: 400,
-      message: "Not enough money"
-    };
-  } else {
-    this.wallet.NETCOIN = Number(this.wallet.NETCOIN) - Number(amount);
-    toUser.wallet.NETCOIN = Number(toUser.wallet.NETCOIN) + Number(amount);
-    await toUser.save();
-    return await this.save();
-  }
+    if (!isEnoughMoney) {
+        throw {
+            code: 400,
+            message: 'Not enough money',
+        };
+    } else {
+        this.wallet.NETCOIN = Number(this.wallet.NETCOIN) - Number(amount);
+        toUser.wallet.NETCOIN = Number(toUser.wallet.NETCOIN) + Number(amount);
+        await toUser.save();
+        const savedProfile = await this.save();
+
+        return {
+            name: savedProfile.name,
+            wallet: savedProfile.wallet,
+            username: savedProfile.username,
+        };
+    }
 };
 
 ProfileSchema.methods.convertCurrency = async function({
-  fromCurrency,
-  targetCurrency,
-  targetAmount,
-  stocks
+    fromCurrency,
+    targetCurrency,
+    targetAmount,
+    stocks,
 }) {
-  const allowedCurrencies = ["RUB", "USD", "EUR", "NETCOIN"];
+    const allowedCurrencies = ['RUB', 'USD', 'EUR', 'NETCOIN'];
 
-  if (allowedCurrencies.indexOf(fromCurrency) === -1) {
-    throw {
-      code: 400,
-      message: `Currency ${fromCurrency} is not one of allowed currencies: ${allowedCurrencies}`
+    if (allowedCurrencies.indexOf(fromCurrency) === -1) {
+        throw {
+            code: 400,
+            message: `Currency ${fromCurrency} is not one of allowed currencies: ${allowedCurrencies}`,
+        };
+    }
+    if (allowedCurrencies.indexOf(targetCurrency) === -1) {
+        throw {
+            code: 400,
+            message: `Currency ${fromCurrency} is not one of allowed currencies: ${allowedCurrencies}`,
+        };
+    }
+    if (!(fromCurrency === 'NETCOIN' || targetCurrency === 'NETCOIN')) {
+        throw {
+            code: 400,
+            message: 'NETCOIN should be one of currencies to convert',
+        };
+    }
+
+    const exchangeType = `${fromCurrency}_${targetCurrency}`;
+
+    const exchangeRate = stocks[exchangeType];
+
+    const fromCurrencyAmount = this.checkAmount(fromCurrency);
+
+    const shouldPay = targetAmount / exchangeRate;
+
+    const isEnoughMoney = fromCurrencyAmount - shouldPay >= 0;
+
+    if (!isEnoughMoney) {
+        throw {
+            code: 406,
+            message: 'Not enough money to complete the operation',
+        };
+    }
+
+    this.wallet[fromCurrency] = Number(this.wallet[fromCurrency]) - Number(shouldPay);
+    this.wallet[targetCurrency] = Number(this.wallet[targetCurrency]) + Number(targetAmount);
+
+    const savedProfile = await this.save();
+    return {
+        name: savedProfile.name,
+        wallet: savedProfile.wallet,
+        username: savedProfile.username,
     };
-  }
-  if (allowedCurrencies.indexOf(targetCurrency) === -1) {
-    throw {
-      code: 400,
-      message: `Currency ${fromCurrency} is not one of allowed currencies: ${allowedCurrencies}`
-    };
-  }
-  if (!(fromCurrency === "NETCOIN" || targetCurrency === "NETCOIN")) {
-    throw {
-      code: 400,
-      message: "NETCOIN should be one of currencies to convert"
-    };
-  }
-
-  const exchangeType = `${fromCurrency}_${targetCurrency}`;
-
-  const exchangeRate = stocks[exchangeType];
-
-  const fromCurrencyAmount = this.checkAmount(fromCurrency);
-
-  const shouldPay = targetAmount / exchangeRate;
-
-  const isEnoughMoney = fromCurrencyAmount - shouldPay >= 0;
-
-  if (!isEnoughMoney) {
-    throw {
-      code: 406,
-      message: "Not enough money to complete the operation"
-    };
-  }
-
-  this.wallet[fromCurrency] =
-    Number(this.wallet[fromCurrency]) - Number(shouldPay);
-  this.wallet[targetCurrency] =
-    Number(this.wallet[targetCurrency]) + Number(targetAmount);
-
-  return await this.save();
 };
 
-const Profile = mongoose.model("Profile", ProfileSchema);
+const Profile = mongoose.model('Profile', ProfileSchema);
 
 async function registerNew(user) {
-  const userExists =
-    (await Profile.find({ username: user.username })).length > 0;
-  if (userExists) {
-    throw {
-      code: 409,
-      message: "User already exists"
-    };
-  }
-  user.salt = cryptoRandomString(16);
-  const hash = createHash("sha1");
-  hash.update(user.password + user.salt);
-  user.password = hash.digest().toString("hex");
-  const newUser = new Profile(user);
-  return await newUser.save();
+    const userExists = (await Profile.find({ username: user.username })).length > 0;
+    if (userExists) {
+        throw {
+            code: 409,
+            message: 'User already exists',
+        };
+    }
+    user.salt = cryptoRandomString(16);
+    const hash = createHash('sha1');
+    hash.update(user.password + user.salt);
+    user.password = hash.digest().toString('hex');
+    const newUser = new Profile(user);
+    return await newUser.save();
 }
 
 async function checkPassword({ username, password }) {
-  const profile = await Profile.findOne({ username });
-  const salt = profile.salt;
-  const hash = createHash("sha1");
-  hash.update(password + salt);
-  password = hash.digest().toString("hex");
-  const passwordCorrect = profile.password === password;
-  if (!passwordCorrect) {
-    throw {
-      code: 401,
-      message: "Password is incorrect"
-    };
-  } else {
-    return {
-      passwordCorrect: true
-    };
-  }
+    const profile = await Profile.findOne({ username });
+    if (!profile) {
+        throw {
+            code: 401,
+            message: 'Login or password is incorrect',
+        };
+    }
+    const salt = profile.salt;
+    const hash = createHash('sha1');
+    hash.update(password + salt);
+    password = hash.digest().toString('hex');
+    const passwordCorrect = profile.password === password;
+    if (!passwordCorrect) {
+        throw {
+            code: 401,
+            message: 'Login or password is incorrect',
+        };
+    } else {
+        return {
+            passwordCorrect: true,
+        };
+    }
 }
 
 module.exports = Profile;

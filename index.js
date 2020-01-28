3b87146cf465f776c78bdb0207a316673c91cf08
Vladimir_Chebukin <Vladimir_Chebukin@epam.com>
2020-01-28 12:42:14 +0300

    Add authorization

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
-const { PORT, PUBLIC_PATH, INDEX_FILE } = process.env;
+const { PORT, PUBLIC_PATH, INDEX_FILE, HOME_FILE } = process.env;
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync', {
@@ -15,16 +17,22 @@ if(!db.get('users').value())
     setDefaultUser(db);
 
 const app = express();
+app.use(cookieParser());
+app.use(session({secret: 'your secret here'}));
 app.use(morgan('tiny'));
 app.use(express.static(`${__dirname}/${PUBLIC_PATH}`));
 
 const api = require('./routes');
-app.use('/', api);
 app.use(bodyParser.json());
+app.use('/', api);
 app.use(morgan('tiny'));
 
-app.get('*', function (_, res) {
-    res.sendFile(path.resolve(`${__dirname}/${PUBLIC_PATH}`, INDEX_FILE));
+app.get('*', function (request, response) {
+    if(request.session.authorized){
+      response.sendFile(path.resolve(`${__dirname}/${PUBLIC_PATH}`, HOME_FILE));
+    }else{
+      response.sendFile(path.resolve(`${__dirname}/${PUBLIC_PATH}`, INDEX_FILE));
+    }
 });
 
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
@@ -35,16 +43,14 @@ function setDefaultUser(database){
         users: [
           { 
             created_at: "2019-10-15T05:28:25.593Z", 
-            name: "Oleg", 
-            email: "Oleg@demo.ru", 
+            login: "oleg@demo.ru", 
             password: "demo", 
             id: "1", 
             isAuthorized: false,
             balance: {RUB: 1000, USD: 20, EUR: 20, NTC: 3000}
           },{ 
             created_at: "2019-11-16T05:28:25.593Z", 
-            name: "Ivan", 
-            email: "ivan@demo.ru", 
+            login: "ivan@demo.ru", 
             password: "demo", 
             id: "2", 
             isAuthorized: false,
@@ -52,8 +58,7 @@ function setDefaultUser(database){
             balance: {RUB: 20, USD: 0, EUR: 0, NTC: 100}
           },{ 
             created_at: "2019-5-14T05:28:25.593Z", 
-            name: "Petr", 
-            email: "petr@demo.ru", 
+            login: "petr@demo.ru", 
             password: "demo", 
             id: "3", 
             isAuthorized: false,
